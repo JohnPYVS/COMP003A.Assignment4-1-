a41f7cc1b2a8d4c8f2b6f9f64b370b8e1aa35692
JohnVisual <johnpatrickdeguzman149@gmail.com>
2026-02-04 20:24:45 -0800

    Add project files.

---
+++ COMP003A.Assignment4(1)/Program.cs
@@ -0,0 +1,10 @@
+﻿namespace COMP003A.Assignment4_1_
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello, World!");
+        }
+    }
+}
